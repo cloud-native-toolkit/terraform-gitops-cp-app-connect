0b681403844acc6a5787439d6c41e93578dcad74
Sean Sundberg <seansund@us.ibm.com>
2021-08-23 11:36:34 -0500

    Intial version
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- variables.tf
+++ variables.tf
@@ -67,24 +67,6 @@ variable "namespace" {
   description = "The namespace where the application should be deployed"
 }
 
-variable "cluster_ingress_hostname" {
-  type        = string
-  description = "Ingress hostname of the IKS cluster."
-  default     = ""
-}
-
-variable "cluster_type" {
-  type        = string
-  description = "The cluster type (openshift or ocp3 or ocp4 or kubernetes)"
-  default     = "ocp4"
-}
-
-variable "tls_secret_name" {
-  type        = string
-  description = "The name of the secret containing the tls certificate values"
-  default     = ""
-}
-
 variable "kubeseal_cert" {
   type        = string
   description = "The certificate/public key used to encrypt the sealed secrets"
@@ -95,4 +77,21 @@ variable "server_name" {
   type        = string
   description = "The name of the server"
   default     = "default"
+}
+
+variable "catalog" {
+  type        = string
+  description = "The catalog source that should be used to deploy the operator"
+}
+
+variable "catalog_namespace" {
+  type        = string
+  description = "The namespace where the catalog has been deployed"
+  default     = "openshift-marketplace"
+}
+
+variable "channel" {
+  type        = string
+  description = "The channel that should be used to deploy the operator"
+  default     = "v1.1-eus"
 }
