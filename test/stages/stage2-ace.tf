2e8887d5752877314a97adad8945eb9df1e3bcbf
Sean Sundberg <seansund@us.ibm.com>
2021-09-11 00:37:13 -0500

    Removes unused kubeseal_cert variable (#2)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-ace.tf
+++ test/stages/stage2-ace.tf
@@ -4,7 +4,6 @@ module "ace" {
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
-  kubeseal_cert = module.argocd-bootstrap.sealed_secrets_cert
   catalog = module.cp_catalogs.catalog_ibmoperators
   platform_navigator_name = module.cp_platform_navigator.name
 }
--- variables.tf
+++ variables.tf
@@ -68,12 +68,6 @@ variable "namespace" {
   default     = "openshift-operators"
 }
 
-variable "kubeseal_cert" {
-  type        = string
-  description = "The certificate/public key used to encrypt the sealed secrets"
-  default     = ""
-}
-
 variable "server_name" {
   type        = string
   description = "The name of the server"
