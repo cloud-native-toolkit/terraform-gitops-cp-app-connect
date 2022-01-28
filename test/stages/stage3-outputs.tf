21980cd257f773021b88ca2bc902e6c193ccab12
Sean Sundberg <seansund@us.ibm.com>
2022-01-28 00:38:13 -0600

    Adds outputs
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ test/stages/stage3-outputs.tf
@@ -0,0 +1,20 @@
+
+resource local_file write_outputs {
+  filename = "gitops-output.json"
+
+  content = jsonencode({
+    name        = module.ace.name
+    branch      = module.ace.branch
+    namespace   = module.ace.namespace
+    server_name = module.ace.server_name
+    layer       = module.ace.layer
+    layer_dir   = module.ace.layer == "infrastructure" ? "1-infrastructure" : (module.ace.layer == "services" ? "2-services" : "3-applications")
+    type        = module.ace.type
+  })
+}
+
+resource local_file write_bin_dir {
+  filename = ".bin_dir"
+
+  content = module.ace.bin_dir
+}
