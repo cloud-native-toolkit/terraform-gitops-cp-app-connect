09a20660d1c90cc16e714ab296f4390f116f06c2
Sean Sundberg <seansund@us.ibm.com>
2022-01-28 01:04:32 -0600

    Updates test to validate subscription and csv (#4)
    * Updates test to validate subscription and csv
    * Updates metadata
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -2,6 +2,7 @@
 
 GIT_REPO=$(cat git_repo)
 GIT_TOKEN=$(cat git_token)
+BIN_DIR=$(cat .bin_dir)
 
 export KUBECONFIG=$(cat .kubeconfig)
 NAMESPACE="openshift-operators"
@@ -37,3 +38,35 @@ cat "payload/2-services/namespace/${NAMESPACE}/${COMPONENT_NAME}/values.yaml"
 
 cd ..
 rm -rf .testrepo
+
+SUBSCRIPTION="subscription/ibm-ace"
+count=0
+until kubectl get "${SUBSCRIPTION}" -n "${NAMESPACE}" || [[ $count -eq 20 ]]; do
+  echo "Waiting for ${SUBSCRIPTION} in ${NAMESPACE}"
+  count=$((count + 1))
+  sleep 15
+done
+
+if [[ $count -eq 20 ]]; then
+  echo "Timed out waiting for ${SUBSCRIPTION} in ${NAMESPACE}"
+  kubectl get subscription -n "${NAMESPACE}"
+  exit 1
+fi
+
+CSV_NAME="ibm-appconnect"
+
+count=0
+until kubectl get csv -n "${NAMESPACE}" -o json | "${BIN_DIR}/jq" -r '.items[] | .metadata.name' | grep -q "${CSV_NAME}" || [[ $count -eq 20 ]]; do
+  echo "Waiting for ${CSV_NAME} csv in ${NAMESPACE}"
+  count=$((count + 1))
+  sleep 15
+done
+
+if [[ $count -eq 20 ]]; then
+  echo "Timed out waiting for ${CSV_NAME} csv in ${NAMESPACE}"
+  kubectl get csv -n "${NAMESPACE}"
+  exit 1
+fi
+
+CSV=$(kubectl get csv -n "${NAMESPACE}" -o json | "${BIN_DIR}/jq" -r '.items[] | .metadata.name' | grep "${CSV_NAME}")
+echo "Found csv ${CSV} in ${NAMESPACE}"
